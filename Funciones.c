a4f47d0f71506dcd34d7a8a88f96aca2ca98a188
gvuono <82596693+gvuono@users.noreply.github.com>
2025-05-25 21:30:16 -0300

    Update Funciones.c

--- Funciones.c
+++ Funciones.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
 #include <semaphore.h>
+#include <string.h>     // <-- para memset, strcmp
+#include <signal.h>
 #include <time.h>
+
 
 #include "funciones.h"
 
